168dc39c6ab740d1d06586d7b025a550eeaa6d05
cterhungga26042006 <cterhungga2006@gmail.com>
2026-04-19 03:07:57 +0700

    Update product.py

--- product.py
+++ product.py
@@ -1,2 +1,9 @@
 import pandas as pd 
-df_product = pd.read_csv('dataset/products.csv')
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+
+DATA_DIR  = 'dataset/'
+TRAIN_FILE = DATA_DIR + 'sales.csv'
+OUT_FILE   = DATA_DIR + 'submission.csv'
