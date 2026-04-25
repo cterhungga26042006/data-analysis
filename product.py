266232ffb48111350e960adb5aebc6d4b41bb54e
cterhungga26042006 <cterhungga2006@gmail.com>
2026-04-25 12:20:43 +0700

    Delete product.py

--- product.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-import numpy as np
-import matplotlib.pyplot as plt
-import warnings
-warnings.filterwarnings('ignore')
-
-DATA_DIR  = 'dataset/'
-TRAIN_FILE = DATA_DIR + 'sales.csv'
-OUT_FILE   = DATA_DIR + 'submission.csv'
